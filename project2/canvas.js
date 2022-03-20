fc31a3dbb65eceaa8146166cfa710812bb62b3c8
RohanYadav5 <98342900+RohanYadav5@users.noreply.github.com>
2022-03-20 23:46:27 +0530

    Update canvas.js

--- project2/canvas.js
+++ project2/canvas.js
@@ -52,12 +52,12 @@ function Circles(x,y,radius,dx,dy){
 
 var circleArray=[];
 
-for(let i=0;i<200;i++){
+for(let i=0;i<250;i++){
     var x=Math.random() * innerWidth;
     var y=Math.random() * innerHeight;
     var dx=(Math.random() - 0.5)*8;
     var dy=(Math.random() - 0.5)*8;
-    var radius=Math.floor(Math.random() * 30) + 1
+    var radius=Math.floor(Math.random() * 7) + 2;
     circleArray.push(new Circles(x,y,radius,dx,dy))
 }
 
